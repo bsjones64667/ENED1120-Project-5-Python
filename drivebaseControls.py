bfbfee2ac3c0db54c45bf34e514ae373bcf9129f
Brandon Jones <bsjones64667@gmail.com>
2020-02-17 20:01:40 -0500

    First Commit

---
+++ drivebaseControls.py
@@ -0,0 +1,34 @@
+from pybricks import ev3brick as brick
+from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor,
+                                 InfraredSensor, UltrasonicSensor, GyroSensor)
+from pybricks.parameters import (Port, Stop, Direction, Button, Color,
+                                 SoundFile, ImageFile, Align)
+from pybricks.tools import print, wait, StopWatch
+from pybricks.robotics import DriveBase
+
+import math
+# Main code for drivebase controls
+class drivebaseControls(object):
+
+    def __init__(self, drivebase):
+        self.drivebase = drivebase
+        # Pull data from drivebase var for use in future 
+        self.lMotor = self.drivebase.left_motor
+        self.rMotor = self.drivebase.right_motor
+        self.wheelCirc = self.drivebase.wheel_diameter * math.pi
+
+    def Turn180(self):
+        self.lMotor.reset_angle(0)
+        self.drivebase.drive(0, 60)
+        while self.lMotor.angle() < 555:
+            pass
+        self.drivebase.stop(Stop.BRAKE)
+        wait(1000)
+
+    def DriveDist(self, distance): # distance in mm
+        targetRots = distance / self.wheelCirc # number of rotations needed to reach target distance
+        targetRots *= 360 # convert rotations to degrees
+        while self.lMotor.angle() < targetRots or self.rMotor.angle() < targetRots:
+            self.drivebase.drive(100, 0)
+        self.drivebase.stop(Stop.BRAKE)
+        wait(1000)
---
+++ main.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env pybricks-micropython
+
+from pybricks import ev3brick as brick
+from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor,
+                                 InfraredSensor, UltrasonicSensor, GyroSensor)
+from pybricks.parameters import (Port, Stop, Direction, Button, Color,
+                                 SoundFile, ImageFile, Align)
+from pybricks.tools import print, wait, StopWatch
+from pybricks.robotics import DriveBase
+from drivebaseControls import *
+
+# Variables to change based on subtask given values, edit as needed
+dist1A = 100 # distance for travel, in mm
+n1A = 10 # number of steps to complete subtask a
+dist1B = [100, 100, 100, 100] # distances to travel in order of occurance, add or remove list values as needed, in mm
+
+# Initialize drivebase vars
+leftDrive = Motor(Port.C, Direction.COUNTERCLOCKWISE)
+rightDrive = Motor(Port.B, Direction.COUNTERCLOCKWISE)
+wheelDiameter = 68.8 # mm
+axelLength = 130 # mm, distance between center of wheels
+
+# Initialize drivebase
+robot = DriveBase(leftDrive, rightDrive, wheelDiameter, axelLength)
+
+# Initialize drivebase controls
+driveControls = drivebaseControls(robot)
+
+
+# Take input from user for which subtask to run
+currTask = -1 # error subtask, should be either 1.1 or 1.2
+brick.display.clear()
+brick.display.text("Left Button: Subtask 1A", (60, 50))
+brick.display.text("Right Button: Subtask 1B")
+brick.display.text("Middle Button to Confirm Selection")
+
+while not Button.CENTER in brick.buttons():
+    if Button.LEFT in brick.buttons():
+        currTask = 1.1
+    if Button.RIGHT in brick.buttons():
+        currTask = 1.2
+
+if currTask == 1.1:
+    subtask1A(dist1A, n1A)
+elif currTask == 1.2:
+    subtask1B(dist1B)
+else:
+    brick.display.clear()
+    brick.display.text("Error, no task selected. Rerun program.", (60, 50))
+
+
+# Subtask 1A
+def subtask1A(distance, n):
+    sign = 1 # sign of direction for loop,
+    for i in range(n):
+        driveControls.DriveDist(dist1A * sign)
+        sign *= -1
+
+# Subtask 1B
+def subtask1B(distance):
+    for dist in distance:
+        for i in range(2):
+            driveControls.DriveDist(dist)
+            driveControls.Turn180()
+
+
+
+
+
+
+
+
