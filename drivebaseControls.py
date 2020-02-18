1333b2607fd243caf18f3a1e2651cd4b90981d5b
Brandon Jones <bsjones64667@gmail.com>
2020-02-18 10:10:44 -0500

    Documented code, changed minor errors. Needs to have gyro attached so current turn code can be replaced.

--- drivebaseControls.py
+++ drivebaseControls.py
@@ -7,54 +7,104 @@ from pybricks.tools import print, wait, StopWatch
 from pybricks.robotics import DriveBase
 
 import math
-# Main code for drivebase controls
+
+
+""" Main code for drivebase controls """
 class drivebaseControls(object):
 
-    def __init__(self, drivebase):
+
+    def __init__(self, drivebase): # TODO: Add gyro input once installed
+
+        # Initialize drivebase for controls
         self.drivebase = drivebase
+
         # Pull data from drivebase var for use in future 
         self.lMotor = self.drivebase.left_motor
         self.rMotor = self.drivebase.right_motor
         self.wheelCirc = self.drivebase.wheel_diameter * math.pi
 
-    def Turn180(self):
-        self.rMotor.reset_angle(0)
-        self.drivebase.drive(0, 60)
-        while self.rMotor.angle() > -555:
-            pass
+
+    """ Function to brake drive base for 1 second, written for repetitive use of code lines """
+    def __BrakeStop(self):
+
+        # Brake for 1 second
         self.drivebase.stop(Stop.BRAKE)
         wait(1000)
 
+
+    """ Turn 180 based on motor angle 
+        (will be replaced by gyro code, not consistent, based on friction of surface) """
+    def Turn180(self):
+
+        # Reset angle of rotation check motor
+        self.rMotor.reset_angle(0)
+
+        # Continue driving until angle has reached turn value
+        self.drivebase.drive(0, 60)
+        while self.rMotor.angle() > -555: # arbitrary value, not consistent
+            pass
+
+        # Brake
+        self.__BrakeStop()
+    
+
+    """ New code for turn 180 once gyro is installed """
+    """
+    def Turn180(self)
+
+        # Reset gyro angle to 0
+        self.gyro.reset_angle(0)
+
+        # Move until desired angle is reached
+        self.drivebase.drive(0, 60)
+        while self.gyro.angle() < 160: # A little under 180 as gyro measures angle late, mess with this value for consistency
+            pass
+
+        # Brake and 1 second delay to ensure stop
+        self.__BrakeStop()
+    """
+
+
+    """ Drive a certain distance, based on rotations of the motor and circumference of wheel """
     def DriveDist(self, distance): # distance in mm
+
+        # Reset distance motor angle
         self.lMotor.reset_angle(0)
+
+        # Calculate target angle to rotate towards
         targetRots = distance / self.wheelCirc # number of rotations needed to reach target distance
         targetRots *= 360 # convert rotations to degrees
+
+        # Move either forward or backwards based on distance sign until angle is reached
         if distance < 0:
             self.drivebase.drive(-100, 0)
             while self.lMotor.angle() > -1 * targetRots:
                 pass
-            self.drivebase.stop(Stop.BRAKE)
-            wait(1000)
         else:
             self.drivebase.drive(100, 0)
             while self.lMotor.angle() < targetRots:
                 pass
-            self.drivebase.stop(Stop.BRAKE)
-            wait(1000)
 
-    """ Function specifically for subtask 1A"""
+        # Brake
+        self.__BrakeStop()
+
+
+    """ Function specifically for subtask 1A, keeps original forward and backward positions 
+        determined based on distance to travel """
     def DriveDistRepeat(self, distance, forwardAngle, backwardAngle):
+
+        # Use forward and backward angles inputted and travel to those based on sign of distance
         if distance < 0:
             self.drivebase.drive(-100, 0)
-            while self.lMotor.angle() > backwardAngle:
+            while self.lMotor.angle() > backwardAngle - 10: # subtract value to account for error
                 pass
-            self.drivebase.stop(Stop.BRAKE)
-            wait(1000)
         else:
             self.drivebase.drive(100, 0)
-            while self.lMotor.angle() < forwardAngle - 20:
+            while self.lMotor.angle() < forwardAngle - 10: # subtract value to account for error
                 pass
-            self.drivebase.stop(Stop.BRAKE)
-            wait(1000)
+
+        # Brake
+        self.__BrakeStop()
+            
 
 
--- main.py
+++ main.py
@@ -9,19 +9,23 @@ from pybricks.tools import print, wait, StopWatch
 from pybricks.robotics import DriveBase
 from drivebaseControls import *
 
+
 """ Variables to change based on subtask given values, edit as needed """
 dist1A = 304.8 # distance for travel, in mm
 n1A = 10 # number of steps to complete subtask a
 dist1B = [100, 500, 300, 1000] # distances to travel in order of occurance, add or remove list values as needed, in mm
 
+
 """ Initialize drivebase variables """
-leftDrive = Motor(Port.C, Direction.COUNTERCLOCKWISE)
+leftDrive = Motor(Port.C, Direction.COUNTERCLOCKWISE) 
 rightDrive = Motor(Port.B, Direction.COUNTERCLOCKWISE)
 wheelDiameter = 75 # mm
 axelLength = 130 # mm, distance between center of wheels
 
+
 """ Initialize drivebase """
 robot = DriveBase(leftDrive, rightDrive, wheelDiameter, axelLength)
+
 
 """ Initialize drivebase controls """
 driveControls = drivebaseControls(robot)
@@ -29,16 +33,25 @@ driveControls = drivebaseControls(robot)
 
 """ Subtask 1A """
 def subtask1A(distance, n):
+
+    # Set up continuous loop based on distance and number of steps
     sign = 1 # sign of direction for loop,
     forwardAng = (distance / driveControls.wheelCirc) * 360
     backwardAng = 0
+
+    # Repeatedly drive n times for target distance
     for i in range(n):
         driveControls.DriveDistRepeat(dist1A * sign, forwardAng, backwardAng)
         sign *= -1
 
+
 """ Subtask 1B """
 def subtask1B(distance):
+
+    # Based on distance array
     for dist in distance:
+
+        # Go for specified distance, then turn around, repeat
         for i in range(2):
             driveControls.DriveDist(dist)
             driveControls.Turn180()
@@ -56,17 +69,19 @@ brick.display.text("L Button: Subtask 1A", (0, 50))
 brick.display.text("R Button: Subtask 1B")
 brick.display.text("M Button to Confirm")
 
+# Keep scanning for input until center button is pressed
 while not Button.CENTER in brick.buttons():
     if Button.LEFT in brick.buttons():
         currTask = 1.1
     if Button.RIGHT in brick.buttons():
         currTask = 1.2
 
+# Based on button input, execute task
 if currTask == 1.1:
     subtask1A(dist1A, n1A)
 elif currTask == 1.2:
     subtask1B(dist1B)
-else:
+else: # error, nothing executed
     brick.display.clear()
     brick.display.text("Error, no task selected.", (0, 50))
     brick.display.text("Rerun program.")
